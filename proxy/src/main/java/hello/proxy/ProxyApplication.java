6f791be2747f3d9b7f75ba0c01dc83574d16a395
Pipi-Olo <Copy.H13@gmail.com>
2022-05-13 22:52:34 +0900

    Add - AppConfig V1

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,8 +1,11 @@
 package hello.proxy;
 
+import hello.proxy.config.AppV1Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
+@Import(AppV1Config.class) // scanBasePackages 로 인해 AppV1Config 가 스프링 빈으로 등록이 안 됨
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
